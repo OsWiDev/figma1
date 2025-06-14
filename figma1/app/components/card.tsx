e01167ceb106332431cb00264db06d33e630ffd7
OsWiDev <oswielcrack@gmail.com>
2025-06-14 09:43:24 -0400

    Componente card.tsx creado

---
+++ figma1/app/components/card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Image from 'next/image'
+
+const GymCard = () => {
+  return (
+    <div>
+        <img src="/public/puxada.jpg" alt="puxada" />
+        <p></p>
+        <p></p>
+        <img src="/public/Icons.png" alt="info" />
+    </div>
+  )
+}
+
+export default GymCard
